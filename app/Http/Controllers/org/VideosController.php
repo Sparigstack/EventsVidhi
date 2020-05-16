55ad50880bf0716a05058adc71fce981fd14b171
ronakproto <ronak@protocrm.com>
2020-05-16 17:18:28 +0530

    latest pull

--- app/Http/Controllers/org/VideosController.php
+++ app/Http/Controllers/org/VideosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\org;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class VideosController extends Controller {
 
