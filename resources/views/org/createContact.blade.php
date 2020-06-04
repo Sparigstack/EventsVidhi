31b6b0f93c039a7926464ef834e05f516a2565b5
ronakproto <ronak@protocrm.com>
2020-06-04 21:29:39 +0530

    improvements in add event page

--- resources/views/org/createContact.blade.php
+++ resources/views/org/createContact.blade.php
@@ -2,6 +2,7 @@
 @section('css')
 <link href="{{ asset('assets/plugins/jquery-multi-select/multi-select.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/plugins/datetimepicker-master/jquery.datetimepicker.css') }}" rel="stylesheet">
 @endsection
 @section('content')
 <div class="container-fluid">
@@ -255,7 +256,7 @@
 
 <script src="{{ asset('assets/plugins/jquery-multi-select/jquery.multi-select.js') }}"></script> -->
 <script src="{{ asset('assets/plugins/select2/js/select2.min.js') }}"></script>
-
+<script src="{{ asset('assets/plugins/datetimepicker-master/jquery.datetimepicker.js') }}"></script>
 <script>
     (function() {
 
@@ -321,55 +322,13 @@
         });
 
 
-        //multiselect start
 
-        $('#my_multi_select1').multiSelect();
-        $('#my_multi_select2').multiSelect({
-            selectableOptgroup: true
-        });
-
-        $('#my_multi_select3').multiSelect({
-            selectableHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='search...'>",
-            selectionHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='search...'>",
-            afterInit: function(ms) {
-                var that = this,
-                    $selectableSearch = that.$selectableUl.prev(),
-                    $selectionSearch = that.$selectionUl.prev(),
-                    selectableSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selectable:not(.ms-selected)',
-                    selectionSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selection.ms-selected';
-
-                that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
-                    .on('keydown', function(e) {
-                        if (e.which === 40) {
-                            that.$selectableUl.focus();
-                            return false;
-                        }
-                    });
-
-                that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
-                    .on('keydown', function(e) {
-                        if (e.which == 40) {
-                            that.$selectionUl.focus();
-                            return false;
-                        }
-                    });
-            },
-            afterSelect: function() {
-                this.qs1.cache();
-                this.qs2.cache();
-            },
-            afterDeselect: function() {
-                this.qs1.cache();
-                this.qs2.cache();
-            }
-        });
-
-        $('.custom-header').multiSelect({
-            selectableHeader: "<div class='custom-header'>Selectable items</div>",
-            selectionHeader: "<div class='custom-header'>Selection items</div>",
-            selectableFooter: "<div class='custom-header'>Selectable footer</div>",
-            selectionFooter: "<div class='custom-header'>Selection footer</div>"
-        });
+//        $('.custom-header').multiSelect({
+//            selectableHeader: "<div class='custom-header'>Selectable items</div>",
+//            selectionHeader: "<div class='custom-header'>Selection items</div>",
+//            selectableFooter: "<div class='custom-header'>Selectable footer</div>",
+//            selectionFooter: "<div class='custom-header'>Selection footer</div>"
+//        });
 
 
     });
